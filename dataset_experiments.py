ff595c0b18c2bcaf2f7b03cb4fe2ecbb426dca6b
roni <ronicopul@gmail.com>
2023-05-20 19:22:31 +0300

    adding dataset experiments

--- dataset_experiments.py
+++ dataset_experiments.py
@@ -19,31 +19,6 @@ _EMBEDDINGS_PATH_FORMAT = _DIRECTORY_PATH + 'embeddings/{dataset_name}_embedding
 _DATASETS_PATH_FORMAT = _DIRECTORY_PATH + 'datasets/{dataset_name}.csv'
 
 
-def synthetic_fit_by_explanation_experiment(dim):
-    single_best_results = {}
-    single_ranks = {}
-    for n_clusters in range(5, 21, 5):
-        print(f"dim={dim}, n_clusters={n_clusters}")
-        features, clusters = make_blobs(n_samples=10000,
-                                        n_features=int(dim),
-                                        centers=n_clusters,
-                                        cluster_std=1.5,
-                                        shuffle=True)
-        synthetic_dataset = pd.DataFrame(features)
-        synthetic_dataset.rename(columns={column: str(column) for column in synthetic_dataset.columns}, inplace=True)
-        embedding_dataset = synthetic_dataset.rename(
-            columns={column: column + '_emb' for column in synthetic_dataset.columns}, inplace=False)
-        n_clusters_list = list(range(3, min(2 * n_clusters + 5, 35), 1))
-        scores = eval_range_of_fit_by_explanation(synthetic_dataset, None, embedding_dataset, None,
-                                                  n_clusters_range=n_clusters_list)
-        single_best_results[n_clusters] = int(n_clusters_list[np.argmax(scores)])
-        n_clusters_in_clusters_list = find_index(n_clusters_list, n_clusters)
-        single_ranks[n_clusters] = len(scores) - find_index(sorted(scores), scores[n_clusters_in_clusters_list])
-        print(f"best_n={single_best_results[n_clusters]}, rank={single_ranks[n_clusters]}")
-        del features, clusters, synthetic_dataset, embedding_dataset
-    return single_best_results, single_ranks
-
-
 def fit_by_explanation_experiments(dataset_names, embedding_names, sample_sizes={}):
     n_clusters_list = list(range(3, 25, 1))
     experiments_results = {}
@@ -63,7 +38,7 @@ def fit_by_explanation_experiments(dataset_names, embedding_names, sample_sizes=
                 metrics_scores = explanation_scores_from_dataset(dataset, embedding)
                 max_metric_n_index = np.argmax(np.array(metrics_scores))
                 max_n_metrics = int(n_clusters_list[max_metric_n_index])
-                spearman_corr = float(spearmanr(fit_by_exp_scores, metrics_scores))
+                spearman_corr = float(spearmanr(fit_by_exp_scores, metrics_scores).statistic)
                 rank_in_fit_by_exp = len(fit_by_exp_scores) - find_index(sorted(fit_by_exp_scores),
                                                                          fit_by_exp_scores[max_metric_n_index])
                 experiments_results[dataset_name][embedding_name] = {'metric_max': max_n_metrics,
